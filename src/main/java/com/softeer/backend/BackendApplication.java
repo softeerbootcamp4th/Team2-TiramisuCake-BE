fb8d8313a026060ec2514837e70986a4355ce322
hyeokson <chanhuk1765@gmail.com>
2024-07-29 17:49:18 +0900

    feat: 애노테이션 추가
    - EnableJpaAuditing: JPA Auditing 기능 활성화
    - ConfigurationPropertiesScan: 설정값 자동 스캔 활성화

--- src/main/java/com/softeer/backend/BackendApplication.java
+++ src/main/java/com/softeer/backend/BackendApplication.java
@@ -2,8 +2,12 @@ package com.softeer.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@ConfigurationPropertiesScan
 public class BackendApplication {
 
 	public static void main(String[] args) {

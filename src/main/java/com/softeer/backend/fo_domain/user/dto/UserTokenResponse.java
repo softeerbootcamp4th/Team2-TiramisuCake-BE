e51c3d18c95834c7e1373cf2728590bd0535b413
hyeokson <chanhuk1765@gmail.com>
2024-07-30 15:50:38 +0900

    feat: Jwt 반환 dto 클래스 생성

---
+++ src/main/java/com/softeer/backend/fo_domain/user/dto/UserTokenResponse.java
@@ -0,0 +1,24 @@
+package com.softeer.backend.fo_domain.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class UserTokenResponse {
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'hh:mm:ss")
+    private LocalDateTime expiredTime;
+
+}

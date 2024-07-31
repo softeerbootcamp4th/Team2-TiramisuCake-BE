6d85cb2d0fd406bf93bd1cda5c48f61d8d020673
DrRivaski <leesm990708@gmail.com>
2024-07-31 16:52:50 +0900

    [add] 빌더 추가, 기본 생성자 추가

--- src/main/java/com/softeer/backend/fo_domain/share/domain/ShareInfo.java
+++ src/main/java/com/softeer/backend/fo_domain/share/domain/ShareInfo.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@NoArgsConstructor
 @Table(name = "share_info")
 public class ShareInfo {
     @Id
@@ -22,4 +25,12 @@ public class ShareInfo {
 
     @Column(name = "draw_remain_cnt")
     private Integer drawRemainCnt;
+
+    @Builder
+    public ShareInfo(Integer userId, String sharedUrl, Integer invitedNum, Integer drawRemainCnt) {
+        this.userId = userId;
+        this.sharedUrl = sharedUrl;
+        this.invitedNum = invitedNum;
+        this.drawRemainCnt = drawRemainCnt;
+    }
 }

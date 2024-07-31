6b46903fd8dba47baa77a72f526c2ffdfc8f5f9f
hyeokson <chanhuk1765@gmail.com>
2024-07-31 16:35:00 +0900

    feat: 유저의 권한 정보를 관리하는 Enum 생성

---
+++ src/main/java/com/softeer/backend/global/common/constant/RoleType.java
@@ -0,0 +1,22 @@
+package com.softeer.backend.global.common.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 유저의 권한 정보
+ */
+@Getter
+@RequiredArgsConstructor
+public enum RoleType {
+
+    ROLE_USER("USER_"), // 일반 유저
+    ROLE_ADMIN("ADMIN_"); // 관리자 유저
+
+    String redisKeyPrefix;
+
+    RoleType(String redisKeyPrefix){
+        this.redisKeyPrefix = redisKeyPrefix;
+    }
+
+}

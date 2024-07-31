c0292ad5828ec1d0184b0cb9fa7deaa8dfdc0f7c
DrRivaski <leesm990708@gmail.com>
2024-07-31 23:56:36 +0900

    [add] hashing을 이용해 16글자의 단축 공유 url 생성하는 로직 작성

--- src/main/java/com/softeer/backend/global/util/SharedUrlUtil.java
+++ src/main/java/com/softeer/backend/global/util/SharedUrlUtil.java
@@ -1,4 +1,36 @@
 package com.softeer.backend.global.util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class SharedUrlUtil {
+    public String generateSHA256Hash(String input) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(input.getBytes());
+        return bytesToHex(hash);
+    }
+
+    // byte 배열을 16진수 문자열로 변환
+    public String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : bytes) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    public String generateShortURL(String originalURL) throws NoSuchAlgorithmException {
+        String hash = generateSHA256Hash(originalURL);
+        // 해시 값의 처음 16자를 사용하여 단축 URL 생성
+        return hash.substring(0, 16);
+    }
 }

7fb82e060b73b9a168c9ee09e4b5b70eaeca1e54
DrRivaski <leesm990708@gmail.com>
2024-07-31 16:34:42 +0900

    [add] associate datasource, Table 어노테이션 추가

--- src/main/java/com/softeer/backend/fo_domain/share/domain/ShareInfo.java
+++ src/main/java/com/softeer/backend/fo_domain/share/domain/ShareInfo.java
@@ -3,12 +3,17 @@ package com.softeer.backend.fo_domain.share.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
 @Getter
 @Entity
+@Table(name = "share_info")
 public class ShareInfo {
     @Id
+    @Column(name = "share_info_id")
+    private Integer shareInfoId;
+
     @Column(name = "user_id")
     private Integer userId;
 

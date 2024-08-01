b862707ed803d0329ede21e34faecc9004b9e694
DrRivaski <leesm990708@gmail.com>
2024-08-01 16:51:37 +0900

    feat: 복권 페이지 접속하는 컨트롤러 생성
    - /event/draw로 접속하는 컨트롤러 메서드 생성

---
+++ src/main/java/com/softeer/backend/fo_domain/draw/controller/DrawController.java
@@ -0,0 +1,20 @@
+package com.softeer.backend.fo_domain.draw.controller;
+
+import com.softeer.backend.fo_domain.draw.dto.DrawResponseDto;
+import com.softeer.backend.fo_domain.draw.service.DrawService;
+import com.softeer.backend.global.annotation.AuthInfo;
+import com.softeer.backend.global.common.response.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class DrawController {
+    private final DrawService drawService;
+
+    @GetMapping("/event/draw")
+    public ResponseDto<DrawResponseDto> getDrawPage(@AuthInfo Integer userId) {
+        return drawService.getDrawPage(userId);
+    }
+}

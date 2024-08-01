a3fe877125981c4fb6ed206104b273d0f2fed2b3
DrRivaski <leesm990708@gmail.com>
2024-08-01 17:18:28 +0900

    feat: DrawParticipationInfoRepository에 ParticipationCount 조회하는 메서드 추가
    - findDrawParticipationCountByUserId 추가

--- src/main/java/com/softeer/backend/fo_domain/draw/repository/DrawParticipationInfoRepository.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/repository/DrawParticipationInfoRepository.java
@@ -1,7 +1,12 @@
 package com.softeer.backend.fo_domain.draw.repository;
 
+import com.softeer.backend.fo_domain.draw.domain.DrawParticipationInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface DrawParticipationInfoRepository {
+public interface DrawParticipationInfoRepository extends JpaRepository<DrawParticipationInfo, Integer> {
+    Optional<Integer> findDrawParticipationCountByUserId(Integer userId);
 }

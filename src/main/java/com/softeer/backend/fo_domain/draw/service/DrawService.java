6530836c8ec6e8d39274ef8aa037c31485728aea
DrRivaski <leesm990708@gmail.com>
2024-08-01 17:34:53 +0900

    feat: DrawService 추가
    - 추첨 페이지 접속 시 필요한 정보 반환하는 getDrawPage 메소드 추가
    - 연속참여일수 조회
    - 남은 복권 추첨 기회 조회
    - ResponseDto 만들어서 반환

---
+++ src/main/java/com/softeer/backend/fo_domain/draw/service/DrawService.java
@@ -0,0 +1,43 @@
+package com.softeer.backend.fo_domain.draw.service;
+
+import com.softeer.backend.fo_domain.draw.domain.DrawParticipationInfo;
+import com.softeer.backend.fo_domain.draw.dto.DrawResponseDto;
+import com.softeer.backend.fo_domain.draw.exception.DrawException;
+import com.softeer.backend.fo_domain.draw.repository.DrawParticipationInfoRepository;
+import com.softeer.backend.fo_domain.draw.repository.DrawRepository;
+import com.softeer.backend.fo_domain.share.domain.ShareInfo;
+import com.softeer.backend.fo_domain.share.exception.ShareInfoException;
+import com.softeer.backend.fo_domain.share.repository.ShareInfoRepository;
+import com.softeer.backend.global.common.code.status.ErrorStatus;
+import com.softeer.backend.global.common.response.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DrawService {
+    private final DrawRepository drawRepository;
+    private final DrawParticipationInfoRepository drawParticipationInfoRepository;
+    private final ShareInfoRepository shareInfoRepository;
+
+    public ResponseDto<DrawResponseDto> getDrawPage(Integer userId) {
+        // 참여 정보 (연속참여일수) 조회
+        DrawParticipationInfo drawParticipationInfo = drawParticipationInfoRepository.findDrawParticipationInfoByUserId(userId)
+                .orElseThrow(() -> new DrawException(ErrorStatus._NOT_FOUND));
+
+        // 초대한 친구 수, 복권 기회 조회
+        ShareInfo shareInfo = shareInfoRepository.findShareInfoByUserId(userId)
+                .orElseThrow(() -> new ShareInfoException(ErrorStatus._NOT_FOUND));
+
+        int drawParticipationCount = drawParticipationInfo.getDrawParticipationCount();
+        int invitedNum = shareInfo.getInvitedNum();
+        int remainDrawCount = shareInfo.getRemainDrawCount();
+
+
+        return ResponseDto.onSuccess(DrawResponseDto.builder()
+                .invitedNum(invitedNum)
+                .remainDrawCount(remainDrawCount)
+                .drawParticipationCount(drawParticipationCount)
+                .build());
+    }
+}

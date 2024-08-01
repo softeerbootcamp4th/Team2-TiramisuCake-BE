4a8d1f33f15893f9bd32e0d31e51a8e9b4cf9bc6
DrRivaski <leesm990708@gmail.com>
2024-08-01 15:20:26 +0900

    refactor: 공유 url exception을 처리하도록 수정
    - 예외 발생 시 ShareInfoException을 반환하도록 수정

--- src/main/java/com/softeer/backend/fo_domain/share/service/ShareInfoService.java
+++ src/main/java/com/softeer/backend/fo_domain/share/service/ShareInfoService.java
@@ -1,9 +1,9 @@
 package com.softeer.backend.fo_domain.share.service;
 
 import com.softeer.backend.fo_domain.share.dto.ShareUrlResponseDto;
+import com.softeer.backend.fo_domain.share.exception.ShareInfoException;
 import com.softeer.backend.fo_domain.share.repository.ShareInfoRepository;
 import com.softeer.backend.global.common.code.status.ErrorStatus;
-import com.softeer.backend.global.common.exception.GeneralException;
 import com.softeer.backend.global.common.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,7 +17,7 @@ public class ShareInfoService {
 
     public ResponseDto<ShareUrlResponseDto> getShortenShareUrl(Integer userId) {
         String sharedUrl = shareInfoRepository.findSharedUrlByUserId(userId).orElseThrow(
-                () -> new GeneralException(ErrorStatus._NOT_FOUND)
+                () -> new ShareInfoException(ErrorStatus._NOT_FOUND)
         );
 
         // 만약 DB에 이미 생성된 단축 url이 있다면 반환

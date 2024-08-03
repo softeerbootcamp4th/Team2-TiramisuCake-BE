4c214cbb02187e619709e9d4da5652043718a2f2
hyeokson <chanhuk1765@gmail.com>
2024-08-04 00:35:42 +0900

    feat: User 클래스의 속성 추가

--- src/main/java/com/softeer/backend/fo_domain/user/domain/User.java
+++ src/main/java/com/softeer/backend/fo_domain/user/domain/User.java
@@ -1,4 +1,29 @@
 package com.softeer.backend.fo_domain.user.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@Table(name = "users")
 public class User {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "marketing_consent")
+    private boolean marketingConsent;
 }

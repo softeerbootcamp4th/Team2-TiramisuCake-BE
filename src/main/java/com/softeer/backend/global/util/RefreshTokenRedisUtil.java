a84c9c87054f0ff67f92254e40356eeff271ff80
hyeokson <chanhuk1765@gmail.com>
2024-08-04 00:38:24 +0900

    refactor: 클래스 이름 변경

--- src/main/java/com/softeer/backend/global/util/RefreshTokenRedisUtil.java
+++ src/main/java/com/softeer/backend/global/util/RefreshTokenRedisUtil.java
@@ -5,25 +5,24 @@ import com.softeer.backend.global.common.entity.JwtClaimsDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 
-@Service
+@Component
 @RequiredArgsConstructor
-public class RedisUtil {
+public class RefreshTokenRedisUtil {
     private final StringRedisTemplate stringRedisTemplate;
 
     // key 에 해당하는 데이터 얻어오는 메서드
     public String getData(String key) {
-        ValueOperations<String, String> valueOperations = getStringStringValueOperations();
-        return valueOperations.get(key);
+        return getStringStringValueOperations().get(key);
     }
 
     // key - value 데이터 설정하는 메서드
     public void setData(String key, String value) {
-        ValueOperations<String, String> valueOperations = getStringStringValueOperations();
-        valueOperations.set(key, value);
+        getStringStringValueOperations().set(key, value);
     }
 
     /* key 에 해당하는 데이터 삭제하는 메소드 */
@@ -33,9 +32,8 @@ public class RedisUtil {
 
     /* key 에 해당하는 데이터 만료기간 설정 메소드 */
     public void setDataExpire(String key, String value, Long duration) {
-        ValueOperations<String, String> valueOperations = getStringStringValueOperations();
         Duration expireDuration = Duration.ofSeconds(duration);
-        valueOperations.set(key, value, expireDuration);
+        getStringStringValueOperations().set(key, value, expireDuration);
     }
 
     private ValueOperations<String, String> getStringStringValueOperations() {

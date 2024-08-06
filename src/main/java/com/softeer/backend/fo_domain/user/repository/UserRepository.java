41eba86394719d71dcdb06c84ba7d238e38e5654
hyeokson <chanhuk1765@gmail.com>
2024-08-06 21:07:08 +0900

    feat: 메서드 추가
    - 전화번호에 해당하는 유저가 있는지 확인하는 메서드
    - 전화번호로 유저 객체를 반환하는 메서드

--- src/main/java/com/softeer/backend/fo_domain/user/repository/UserRepository.java
+++ src/main/java/com/softeer/backend/fo_domain/user/repository/UserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    boolean existsByPhoneNumber(String phoneNumber);
+
+    User findByPhoneNumber(String phoneNumber);
 }

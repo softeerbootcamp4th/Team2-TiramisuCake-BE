ce1fa56cdaa975a2a2ba4151c25a901db8d9fe2e
hyeokson <chanhuk1765@gmail.com>
2024-08-06 18:54:52 +0900

    feat: Repository 애노테이션 추가

--- src/main/java/com/softeer/backend/fo_domain/user/repository/UserRepository.java
+++ src/main/java/com/softeer/backend/fo_domain/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.softeer.backend.fo_domain.user.repository;
 
 import com.softeer.backend.fo_domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 }

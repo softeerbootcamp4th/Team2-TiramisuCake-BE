9e2357bcb18f020851fd166c5da98421434dd2ab
hyeokson <chanhuk1765@gmail.com>
2024-08-06 19:03:52 +0900

    feat: 선착순에 사용되는 값 추가
    - 선착순 참여자를 저장하는 변수는 유지하고 user의 primary key 값을 set으로 저장하여 중복된 사용자가 이벤트에 참여하지 못하도록 막기

--- src/main/java/com/softeer/backend/global/util/EventLockRedisUtil.java
+++ src/main/java/com/softeer/backend/global/util/EventLockRedisUtil.java
@@ -1,9 +1,10 @@
 package com.softeer.backend.global.util;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.*;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 /**
  * 선착순, 추첨 이벤트의 동기화를 위해 사용되는 RedisUtil 클래스
@@ -12,29 +13,54 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class EventLockRedisUtil {
 
-    private final RedisTemplate<String, Integer> redisTemplate;
+    private final RedisTemplate<String, Integer> integerRedisTemplate;
 
     // key 에 해당하는 데이터 얻어오는 메서드
-    public Integer getData(String key) {
+    public Integer getParticipantCount(String key) {
         return getStringIntegerValueOperations().get(key);
     }
 
     // key에 해당하는 데이터의 값을 1 더하는 메서드
     // 원자적으로 값을 증가시킨다.
-    public void incrementData(String key){
+    public void incrementParticipantCount(String key){
         getStringIntegerValueOperations().increment(key, 1);
     }
 
     // key - value 데이터 설정하는 메서드
-    public void setData(String key, int value) {
+    public void setParticipantCount(String key, int value) {
         getStringIntegerValueOperations().set(key, value);
     }
 
-    public void deleteData(String key){
-        redisTemplate.delete(key);
+    public void deleteParticipantCount(String key){
+        integerRedisTemplate.delete(key);
+    }
+
+    // 참가자의 ID를 Set으로 저장하고 관리하는 메서드
+    public void addParticipantId(String key, Integer participantId) {
+        getStringSetIntegerValueOperations().add(key, participantId);
+    }
+
+    public boolean isParticipantExists(String key, Integer participantId) {
+        return Boolean.TRUE.equals(getStringSetIntegerValueOperations().isMember(key, participantId));
+    }
+
+    public void removeParticipantId(String key, Integer participantId) {
+        getStringSetIntegerValueOperations().remove(key, participantId);
+    }
+
+    public Set<Integer> getAllParticipantIds(String key) {
+        return getStringSetIntegerValueOperations().members(key);
+    }
+
+    public void deleteParticipantIds(String key){
+        integerRedisTemplate.delete(key);
     }
 
     private ValueOperations<String, Integer> getStringIntegerValueOperations() {
-        return redisTemplate.opsForValue();
+        return integerRedisTemplate.opsForValue();
+    }
+
+    private SetOperations<String, Integer> getStringSetIntegerValueOperations() {
+        return integerRedisTemplate.opsForSet();
     }
 }

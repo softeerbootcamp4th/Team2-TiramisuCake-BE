575354b9ec7b39b9d066fd477dc36f63ae8a2913
hyeokson <chanhuk1765@gmail.com>
2024-08-08 19:56:25 +0900

    refactor: 애노테이션 변경

--- src/main/java/com/softeer/backend/fo_domain/user/controller/LoginController.java
+++ src/main/java/com/softeer/backend/fo_domain/user/controller/LoginController.java
@@ -7,8 +7,8 @@ import com.softeer.backend.global.common.code.status.SuccessStatus;
 import com.softeer.backend.global.common.response.ResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +18,7 @@ public class LoginController {
     private final LoginService loginService;
 
     @PostMapping("/login")
-    ResponseDto<UserTokenResponse> handleLogin(@Valid @ModelAttribute LoginRequest loginRequest){
+    ResponseDto<UserTokenResponse> handleLogin(@Valid @RequestBody LoginRequest loginRequest){
         UserTokenResponse userTokenResponse = loginService.handleLogin(loginRequest);
 
         return ResponseDto.onSuccess(SuccessStatus._LOGIN_SUCCESS, userTokenResponse);

e3535ecd2e253a55f1025fe1773db8284b2204e0
DrRivaski <leesm990708@gmail.com>
2024-08-08 10:44:06 +0900

    Feat: draw 관련 로직 처리하는 DrawUtil 클래스 생성
    - 당첨 이미지 생성 메소드
    - 낙첨 이미지 생성 메소드

---
+++ src/main/java/com/softeer/backend/fo_domain/draw/util/DrawUtil.java
@@ -0,0 +1,15 @@
+package com.softeer.backend.fo_domain.draw.util;
+
+import java.util.List;
+
+public class DrawUtil {
+
+
+    public List<String> generateWinImages() {
+
+    }
+
+    public List<String> generateLostImages() {
+
+    }
+}

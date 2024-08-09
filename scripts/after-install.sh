716e0fb2d6458f28dda121a8bf570b48b014208c
DrRivaski <48974215+DrRivaski@users.noreply.github.com>
2024-08-09 13:57:17 +0900

    after-install.sh 수정

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -3,6 +3,16 @@
 # 경로 설정
 APP_DIR="/home/ubuntu/backend/build/libs"
 YML_PATH="/home/ubuntu/backend/src/main/resources/yml/application-prod.yml"
+PORT=5000
+
+# 포트가 사용 중인지 확인하고, 사용 중이면 해당 프로세스를 종료
+PID=$(lsof -ti:$PORT)
+if [ -n "$PID" ]; then
+  echo "Port $PORT is in use. Killing process $PID..."
+  kill -9 $PID
+else
+  echo "Port $PORT is free."
+fi
 
 # JAR 파일 실행
 JAR_FILE="$APP_DIR/backend-0.0.1-SNAPSHOT.jar"

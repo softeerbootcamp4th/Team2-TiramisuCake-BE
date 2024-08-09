0ccb7d41c4b4a37d6542619a0700528caa737d3e
DrRivaski <leesm990708@gmail.com>
2024-08-09 12:23:38 +0900

    feat: 초기 설정 및 스케줄러 설정
    - 빈이 생성되면 데이터베이스로부터 설정 값 가져오도록 설정
    - 스케줄러를 이용해 매일 새벽 1시에 redis에서 임시 당첨자 목록 삭제하도록 설정

--- src/main/java/com/softeer/backend/fo_domain/draw/service/DrawSettingManager.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/service/DrawSettingManager.java
@@ -1,8 +1,16 @@
 package com.softeer.backend.fo_domain.draw.service;
 
+import com.softeer.backend.fo_domain.draw.domain.DrawSetting;
+import com.softeer.backend.fo_domain.draw.exception.DrawException;
 import com.softeer.backend.fo_domain.draw.repository.DrawSettingRepository;
+import com.softeer.backend.global.common.code.status.ErrorStatus;
+import com.softeer.backend.global.common.constant.RedisLockPrefix;
+import com.softeer.backend.global.util.EventLockRedisUtil;
+import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Component;
 
 import java.sql.Date;
@@ -12,6 +20,8 @@ import java.sql.Date;
 @RequiredArgsConstructor
 public class DrawSettingManager {
     private final DrawSettingRepository drawSettingRepository;
+    private final ThreadPoolTaskScheduler taskScheduler;
+    private final EventLockRedisUtil eventLockRedisUtil;
 
     private Date startTime;
     private Date endTime;
@@ -19,5 +29,29 @@ public class DrawSettingManager {
     private int winnerNum2;
     private int winnerNum3;
 
+    // @PostConstruct로 생성됐을 시 세팅정보 가져오기
+    // 스케줄러로 01:00:00에 redis 임시 목록 삭제하기
 
+    @PostConstruct
+    public void initializeDrawSettingManager() {
+        DrawSetting drawSetting = drawSettingRepository.findById(1)
+                .orElseThrow(() -> new DrawException(ErrorStatus._DRAW_PARTICIPATION_INFO_NOT_FOUND));
+
+        startTime = drawSetting.getStartTime();
+        endTime = drawSetting.getEndTime();
+        winnerNum1 = drawSetting.getWinnerNum1();
+        winnerNum2 = drawSetting.getWinnerNum2();
+        winnerNum3 = drawSetting.getWinnerNum3();
+
+        // 매일 01:00:00에 redis 임시 당첨자 목록 삭제하기
+        taskScheduler.schedule(this::deleteTempWinnerSetFromRedis, new CronTrigger("0 0 1 * * *"));
+    }
+
+    private void deleteTempWinnerSetFromRedis() {
+        String drawTempKey;
+        for (int ranking = 1; ranking < 4; ranking++) {
+            drawTempKey = RedisLockPrefix.DRAW_TEMP_PREFIX.getPrefix() + ranking;
+            eventLockRedisUtil.deleteTempWinnerList(drawTempKey);
+        }
+    }
 }

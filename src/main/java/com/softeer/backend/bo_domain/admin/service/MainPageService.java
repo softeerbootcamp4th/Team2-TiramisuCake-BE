d469144a942893d60571537010e1844b4fc140be
hyeokson <chanhuk1765@gmail.com>
2024-08-11 03:04:31 +0900

    feat: 메인 페이지 service 클래스 구현

---
+++ src/main/java/com/softeer/backend/bo_domain/admin/service/MainPageService.java
@@ -0,0 +1,29 @@
+package com.softeer.backend.bo_domain.admin.service;
+
+import com.softeer.backend.bo_domain.admin.dto.main.MainPageResponseDto;
+import com.softeer.backend.fo_domain.draw.domain.DrawSetting;
+import com.softeer.backend.fo_domain.draw.repository.DrawSettingRepository;
+import com.softeer.backend.fo_domain.fcfs.domain.FcfsSetting;
+import com.softeer.backend.fo_domain.fcfs.repository.FcfsSettingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MainPageService {
+
+    private final FcfsSettingRepository fcfsSettingRepository;
+    private final DrawSettingRepository drawSettingRepository;
+
+    @Transactional(readOnly = true)
+    public MainPageResponseDto getMainPage(){
+        List<FcfsSetting> fcfsSettingList = fcfsSettingRepository.findAll(Sort.by(Sort.Order.asc("round")));
+        List<DrawSetting> drawSetting = drawSettingRepository.findAll();
+
+        return MainPageResponseDto.of(fcfsSettingList, drawSetting);
+    }
+}

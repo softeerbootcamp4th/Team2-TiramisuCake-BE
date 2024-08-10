b8e65f55931278d120d62e261a361c128d9225b9
hyeokson <chanhuk1765@gmail.com>
2024-08-11 03:09:46 +0900

    feat: EventParticipation 조회 메서드 구현

--- src/main/java/com/softeer/backend/bo_domain/eventparticipation/repository/EventParticipationRepository.java
+++ src/main/java/com/softeer/backend/bo_domain/eventparticipation/repository/EventParticipationRepository.java
@@ -3,10 +3,16 @@ package com.softeer.backend.bo_domain.eventparticipation.repository;
 import com.softeer.backend.bo_domain.eventparticipation.domain.EventParticipation;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface EventParticipationRepository extends JpaRepository<EventParticipation, Integer> {
+
+    @Query("SELECT e FROM EventParticipation e WHERE e.eventDate BETWEEN :startDate AND :endDate")
+    List<EventParticipation> findAllByEventDateBetween(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
     default EventParticipation findSingleEventParticipation() {
         List<EventParticipation> results = findAll();

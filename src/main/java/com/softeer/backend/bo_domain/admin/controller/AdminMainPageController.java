e69e8fba0fb5ee679fcbe4e962b302fbbb05bb2b
hyeokson <chanhuk1765@gmail.com>
2024-08-12 02:38:12 +0900

    refactor: 클래스 및 변수명 변경

--- src/main/java/com/softeer/backend/bo_domain/admin/controller/AdminMainPageController.java
+++ src/main/java/com/softeer/backend/bo_domain/admin/controller/AdminMainPageController.java
@@ -1,6 +1,6 @@
 package com.softeer.backend.bo_domain.admin.controller;
 
-import com.softeer.backend.bo_domain.admin.dto.main.MainPageResponseDto;
+import com.softeer.backend.bo_domain.admin.dto.main.AdminMainPageResponseDto;
 import com.softeer.backend.bo_domain.admin.service.AdminMainPageService;
 import com.softeer.backend.global.common.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +15,9 @@ public class AdminMainPageController {
     private final AdminMainPageService adminMainPageService;
 
     @GetMapping("/main")
-    public ResponseDto<MainPageResponseDto> getMainPage() {
-        MainPageResponseDto mainPageResponseDto = adminMainPageService.getMainPage();
+    public ResponseDto<AdminMainPageResponseDto> getMainPage() {
+        AdminMainPageResponseDto adminMainPageResponseDto = adminMainPageService.getMainPage();
 
-        return ResponseDto.onSuccess(mainPageResponseDto);
+        return ResponseDto.onSuccess(adminMainPageResponseDto);
     }
 }

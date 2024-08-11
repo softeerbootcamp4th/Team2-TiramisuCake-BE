769a0152cbf0db9f15f67ef509f66cc7cdd435c8
DrRivaski <leesm990708@gmail.com>
2024-08-11 23:00:32 +0900

    refactor: readOnly false로 변경

--- src/main/java/com/softeer/backend/fo_domain/user/service/LoginService.java
+++ src/main/java/com/softeer/backend/fo_domain/user/service/LoginService.java
@@ -43,7 +43,7 @@ public class LoginService {
      * 3. 전화번호가 이미 User DB에 등록되어 있는 경우, 전화번호로 User 객체를 조회한다.
      * 4. User 객체의 id를 얻은 후에, access & refresh token을 client에게 전달한다.
      */
-    @Transactional(readOnly = true)
+    @Transactional(readOnly = false)
     public JwtTokenResponseDto handleLogin(LoginRequestDto loginRequestDto) {
 
         // 인증번호가 인증 되지 않은 경우, 예외 발생
@@ -113,6 +113,8 @@ public class LoginService {
                 .userId(userId)
                 .shareUrl(shareUrl)
                 .build();
+
+        System.out.println("userId = " + userId);
 
         shareUrlInfoRepository.save(shareUrlInfo);
     }

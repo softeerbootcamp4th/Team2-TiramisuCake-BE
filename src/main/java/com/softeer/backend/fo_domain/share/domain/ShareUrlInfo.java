3ab7a61ededc4cae84590f689328a1156e18948a
DrRivaski <leesm990708@gmail.com>
2024-08-11 22:30:46 +0900

    refactor: ShareUrlInfo에 빌더 어노테이션 추가

--- src/main/java/com/softeer/backend/fo_domain/share/domain/ShareUrlInfo.java
+++ src/main/java/com/softeer/backend/fo_domain/share/domain/ShareUrlInfo.java
@@ -1,10 +1,14 @@
 package com.softeer.backend.fo_domain.share.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@RequiredArgsConstructor
 @Table(name = "share_url_info")
 public class ShareUrlInfo {
     @Id

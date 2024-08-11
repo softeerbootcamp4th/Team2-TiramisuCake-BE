c31441af8ab7a04570b28fd6b71490cf58394e30
hyeokson <chanhuk1765@gmail.com>
2024-08-12 02:40:22 +0900

    refactor: enum 변수명 수정

--- src/main/java/com/softeer/backend/global/staticresources/constant/StaticText.java
+++ src/main/java/com/softeer/backend/global/staticresources/constant/StaticText.java
@@ -1,9 +1,12 @@
 package com.softeer.backend.global.staticresources.constant;
 
+import lombok.Getter;
+
+@Getter
 public enum StaticText {
 
     EVENT_PERIOD("%s ~ %s"),
-    FCFS_INFO("매주 %s, %s %s %s시 선착순 %"),
+    FCFS_INFO("매주 %s, %s %s시 선착순 %s명"),
     FCFS_TITLE("'24시 내 차' 이벤트"),
     FCFS_CONTENT("하단의 The new IONIQ 5 정보를 바탕으로 빠르게 문장 퀴즈를 맞춰\n" +
             "24시간 렌트권과 신차 할인권을 얻을 수 있어요."),
@@ -23,7 +26,7 @@ public enum StaticText {
     INTERIOR_IMAGE_CONTENT("편안한 거주 공간 (Living Space) 테마를 반영하여 더 넓은 실내 공간을 즐길 수 있도록 연출합니다."),
     INTERIOR_OPENNESS_TITLE("개방감"),
     INTERIOR_OPENNESS_SUBTITLE("개방감과 와이드한 이미지 제공"),
-    INTERIOR_OPENNESS_CONENT("심리스 스타일의 12.3인치 LCD 클러스터는 탁월한 개방감으로 즐거운 드라이빙 환경을 제공합니다.\n" +
+    INTERIOR_OPENNESS_CONTENT("심리스 스타일의 12.3인치 LCD 클러스터는 탁월한 개방감으로 즐거운 드라이빙 환경을 제공합니다.\n" +
             "클러스터와 인포테인먼트 시스템에 일체형 커버글래스를 적용하여 와이드한 이미지를 제공합니다."),
 
     INTERIOR_WELLNESS_TITLE("웰니스"),
@@ -37,7 +40,7 @@ public enum StaticText {
     PERFORMANCE_IMAGE_CONTENT("항속형 대용량 배터리를 적용하여 주행 가능 거리를 높였습니다."),
     PERFORMANCE_BRAKING_TITLE("에너지 효율"),
     PERFORMANCE_BRAKING_SUBTITLE("회생 제동 시스템"),
-    PERFORMANCE_BRAKING_CONENT("스티어링 휠의 패들쉬프트를 통해 회생제동 수준을 단계별로 조작할 수 있어\n" +
+    PERFORMANCE_BRAKING_CONTENT("스티어링 휠의 패들쉬프트를 통해 회생제동 수준을 단계별로 조작할 수 있어\n" +
             "브레이크 및 가족 페달 작동을 최소화하여 에너지 효율을 높일 수 있습니다."),
 
     PERFORMANCE_DRIVING_TITLE("주행성능"),
@@ -52,7 +55,7 @@ public enum StaticText {
     CHARGING_IMAGE_CONTENT("차량 외부로 전력을 공급할 수 있는 V2L 기능과 쉽고 빠르게 충전 관련 서비스는 사용자에게 새로운 경험을 제공합니다."),
     CHARGING_FAST_TITLE("초급속 충전"),
     CHARGING_FAST_SUBTITLE("18분 초급속 충전 경험"),
-    CHARGING_FAST_CONENT("400V/800V 멀티 급속 충전 시스템으로 다양한 충전 인프라를 사용할 수 있으며,\n" +
+    CHARGING_FAST_CONTENT("400V/800V 멀티 급속 충전 시스템으로 다양한 충전 인프라를 사용할 수 있으며,\n" +
             "급속 충전기(350kW) 사용 시 18분 이내에 배터리 용량의 10%에서 80%까지 충전이 가능합니다."),
 
     CHARGING_V2L_TITLE("실외/실내\n" +
@@ -67,7 +70,7 @@ public enum StaticText {
     SAFE_IMAGE_CONTENT("다양한 안전, 편의 기술로 편리하고 안전한 드라이빙 환경을 제공합니다."),
     SAFE_DRIVING_TITLE("주행 안전"),
     SAFE_DRIVING_SUBTITLE("도로 주행 중 안전"),
-    SAFE_DRIVING_CONENT("고속도로 및 자동차 전용도로 주행 시 도로 상황에 맞춰 안전한 속도로 주행하도록 도와주며,\n" +
+    SAFE_DRIVING_CONTENT("고속도로 및 자동차 전용도로 주행 시 도로 상황에 맞춰 안전한 속도로 주행하도록 도와주며,\n" +
             "안전속도 구간, 곡선 구간, 진출입로 진입 전에 자동으로 속도를 줄이고 해당 구간을 지나면 원래 설정한 속도로 복귀합니다.\n" +
             "일정 속도 이상으로 주행 시, 스티어링 휠을 잡은 상태에서 방향지시등 스위치를 변경하고자 하는 차로 방향으로 자동으로 움직입니다."),
 

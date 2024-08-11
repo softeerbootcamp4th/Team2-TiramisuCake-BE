1e301e6726695f5e426118031771c2b6adaaf60a
hyeokson <chanhuk1765@gmail.com>
2024-08-12 04:05:54 +0900

    feat: 어드민 로그인 요청 dto 수정

--- src/main/java/com/softeer/backend/bo_domain/admin/dto/login/AdminLoginRequestDto.java
+++ src/main/java/com/softeer/backend/bo_domain/admin/dto/login/AdminLoginRequestDto.java
@@ -1,6 +1,7 @@
 package com.softeer.backend.bo_domain.admin.dto.login;
 
 import com.softeer.backend.global.common.constant.ValidationConstant;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
@@ -10,11 +11,9 @@ import lombok.*;
 @Getter
 public class AdminLoginRequestDto {
 
-    @Pattern(regexp = ValidationConstant.ADMIN_ACCOUNT_REGEX,
-            message = ValidationConstant.ADMIN_ACCOUNT_MSG)
+    @NotNull
     private String account;
 
-    @Pattern(regexp = ValidationConstant.ADMIN_PASSWORD_REGEX,
-            message = ValidationConstant.ADMIN_PASSWORD_MSG)
+    @NotNull
     private String password;
 }

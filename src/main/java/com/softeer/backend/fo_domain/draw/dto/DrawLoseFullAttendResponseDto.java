54d0c00c09d3f37d07920f9b56cb46221ab90e9d
DrRivaski <leesm990708@gmail.com>
2024-08-12 18:02:13 +0900

    refactor: 어노테이션 추가
    - @Data
    - @SuperBuilder
    - @NoArgsConstructor
    - @AllArgsConstructor

--- src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseFullAttendResponseDto.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseFullAttendResponseDto.java
@@ -1,8 +1,15 @@
 package com.softeer.backend.fo_domain.draw.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DrawLoseFullAttendResponseDto extends DrawLoseResponseDto {
     private FullAttendModal fullAttendModal;
 
--- src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseNotAttendResponseDto.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseNotAttendResponseDto.java
@@ -1,4 +1,14 @@
 package com.softeer.backend.fo_domain.draw.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DrawLoseNotAttendResponseDto extends DrawLoseResponseDto {
 }

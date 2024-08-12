b873157ccfc0d15c04625e990c956d1a767d045e
DrRivaski <leesm990708@gmail.com>
2024-08-12 18:31:24 +0900

    refactor: 사용하지 않는 import문 제거

--- src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseFullAttendResponseDto.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseFullAttendResponseDto.java
@@ -1,9 +1,7 @@
 package com.softeer.backend.fo_domain.draw.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
--- src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawWinNotAttendResponseDto.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawWinNotAttendResponseDto.java
@@ -1,8 +1,6 @@
 package com.softeer.backend.fo_domain.draw.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data

232d60734088e5c29775df53bf5c16683e36be10
DrRivaski <leesm990708@gmail.com>
2024-08-12 18:02:00 +0900

    refactor: 어노테이션 추가
    - @Data
    - @SuperBuilder
    - @NoArgsConstructor
    - @AllArgsConstructor

--- src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseResponseDto.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseResponseDto.java
@@ -1,16 +1,22 @@
 package com.softeer.backend.fo_domain.draw.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DrawLoseResponseDto extends DrawResponseDto {
     private LoseModal loseModal; // LoseModal 정보
 
     @Data
-    @Builder
+    @SuperBuilder
     public static class LoseModal {
         private String shareUrl; // 공유 url
     }

390a6cc5a2c1b815dc85352f3a756ca761f18886
DrRivaski <leesm990708@gmail.com>
2024-08-12 18:01:09 +0900

    refactor: @Builder를 @SuperBuilder로 변경

--- src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawResponseDto.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawResponseDto.java
@@ -4,11 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
 @Data
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 public class DrawResponseDto {

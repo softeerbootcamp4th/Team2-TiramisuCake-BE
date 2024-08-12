14ae8963882fce3f315811703c51c33ee5868acc
DrRivaski <leesm990708@gmail.com>
2024-08-12 17:32:31 +0900

    refactor: 사용하지 않는 import문 제거

--- src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseNotAttendResponseDto.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseNotAttendResponseDto.java
@@ -1,10 +1,4 @@
 package com.softeer.backend.fo_domain.draw.dto;
 
-import lombok.Builder;
-import lombok.Data;
-
-import java.util.List;
-
-
 public class DrawLoseNotAttendResponseDto extends DrawLoseResponseDto {
 }
--- src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawWinFullAttendResponseDto.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawWinFullAttendResponseDto.java
@@ -3,8 +3,6 @@ package com.softeer.backend.fo_domain.draw.dto;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.List;
-
 public class DrawWinFullAttendResponseDto extends DrawWinResponseDto {
     private FullAttendModal fullAttendModal;
 

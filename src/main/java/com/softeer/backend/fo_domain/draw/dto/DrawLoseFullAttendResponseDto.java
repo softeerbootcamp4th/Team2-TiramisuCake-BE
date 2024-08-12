a9e34c98a5179977f2ab02aeb7b2073859e2255e
DrRivaski <leesm990708@gmail.com>
2024-08-12 17:30:10 +0900

    feat: 낙첨자인 경우 7일 연속 출석했을 때 응답 dto 추가

---
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseFullAttendResponseDto.java
@@ -0,0 +1,17 @@
+package com.softeer.backend.fo_domain.draw.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+public class DrawLoseFullAttendResponseDto extends DrawLoseResponseDto {
+    private FullAttendModal fullAttendModal;
+
+    @Data
+    @Builder
+    public static class FullAttendModal {
+        private String title; // 제목
+        private String subtitle; // 부제목
+        private String img; // 이미지 URL (S3 URL)
+        private String description; // 설명
+    }
+}

fef00cffbf6eeb78361a3f480a30e4ae9a442dde
DrRivaski <leesm990708@gmail.com>
2024-08-12 17:29:12 +0900

    refactor: 공통 변수 삭제, 낙첨 modal 추가

--- src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseResponseDto.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseResponseDto.java
@@ -6,23 +6,12 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Data
-@NoArgsConstructor
-public class DrawLoseResponseDto implements DrawResponseDto {
-    private int invitedNum; // 내가 초대한 친구 수
-    private int remainDrawCount; // 남은 복권 기회
-    private int drawParticipationCount; // 연속 참여 일수
-    private boolean isDrawWin; // 당첨됐는지 여부
-    private List<String> images; // 이미지 리스트
-    private LoseModal loseModal; // WinModal 정보
+public class DrawLoseResponseDto extends DrawResponseDto {
+    private LoseModal loseModal; // LoseModal 정보
 
+    @Data
     @Builder
-    public DrawLoseResponseDto(int invitedNum, int remainDrawCount, int drawParticipationCount, boolean isDrawWin, List<String> images, LoseModal loseModal) {
-        this.invitedNum = invitedNum;
-        this.remainDrawCount = remainDrawCount;
-        this.drawParticipationCount = drawParticipationCount;
-        this.isDrawWin = isDrawWin;
-        this.images = images;
-        this.loseModal = loseModal;
+    public static class LoseModal {
+        private String shareUrl; // 공유 url
     }
 }

4ecb3e5bee41ae33a01ab4765bf56de667876434
DrRivaski <leesm990708@gmail.com>
2024-08-12 19:18:30 +0900

    refactor: 변수명 변경
    - img -> image

--- src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseFullAttendResponseDto.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseFullAttendResponseDto.java
@@ -14,7 +14,7 @@ public class DrawLoseFullAttendResponseDto extends DrawLoseResponseDto {
     public static class FullAttendModal {
         private String title; // 제목
         private String subtitle; // 부제목
-        private String img; // 이미지 URL (S3 URL)
+        private String image; // 이미지 URL (S3 URL)
         private String description; // 설명
     }
 }
--- src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawWinFullAttendResponseDto.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawWinFullAttendResponseDto.java
@@ -15,7 +15,7 @@ public class DrawWinFullAttendResponseDto extends DrawWinResponseDto {
     public static class FullAttendModal {
         private String title; // 제목
         private String subtitle; // 부제목
-        private String img; // 이미지 URL (S3 URL)
+        private String image; // 이미지 URL (S3 URL)
         private String description; // 설명
     }
 }

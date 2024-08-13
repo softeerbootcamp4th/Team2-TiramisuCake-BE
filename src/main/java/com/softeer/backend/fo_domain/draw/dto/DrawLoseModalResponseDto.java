0f1a5c8efcee451fa812796e718707575d46fede
DrRivaski <leesm990708@gmail.com>
2024-08-13 11:16:31 +0900

    feat: 낙첨 모달 dto 클래스 생성

---
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawLoseModalResponseDto.java
@@ -0,0 +1,10 @@
+package com.softeer.backend.fo_domain.draw.dto;
+
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+public class DrawLoseModalResponseDto {
+    private String shareUrl;
+}

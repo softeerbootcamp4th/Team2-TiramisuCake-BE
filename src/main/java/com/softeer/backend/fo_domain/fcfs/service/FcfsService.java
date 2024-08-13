9d61498eea76fd63fc28a90ff84d57f6f0e86705
hyeokson <chanhuk1765@gmail.com>
2024-08-13 16:08:12 +0900

    chore: 임시로 선착순 기능 주석 처리

--- src/main/java/com/softeer/backend/fo_domain/fcfs/service/FcfsService.java
+++ src/main/java/com/softeer/backend/fo_domain/fcfs/service/FcfsService.java
@@ -26,21 +26,21 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class FcfsService {
 
-    FcfsSettingManager fcfsSettingManager;
-    FcfsRepository fcfsRepository;
-    EventLockRedisUtil eventLockRedisUtil;
-    UserRepository userRepository;
+    private final FcfsSettingManager fcfsSettingManager;
+    private final FcfsRepository fcfsRepository;
+    private final EventLockRedisUtil eventLockRedisUtil;
+    private final UserRepository userRepository;
 
     /**
      * 1. 선착순 당첨자가 아직 다 결정되지 않았으면, 선착순 당첨 응답 생성 및 반환
      * 2. 선착순 당첨자가 다 결정됐다면, Redisson lock을 사용하지 않고 Redis에 저장된 선착순 이벤트 참여자 수를 1명씩 더한다.
      */
-    public FcfsResponseDto handleFcfsEvent(int userId) {
-        if (fcfsSettingManager.isFcfsClosed())
-            return countFcfsParticipant(fcfsSettingManager.getRound());
-
-        return saveFcfsWinners(userId, fcfsSettingManager.getRound());
-    }
+//    public FcfsResponseDto handleFcfsEvent(int userId) {
+//        if (fcfsSettingManager.isFcfsClosed())
+//            return countFcfsParticipant(fcfsSettingManager.getRound());
+//
+//        return saveFcfsWinners(userId, fcfsSettingManager.getRound());
+//    }
 
     /**
      * 1. Redisson lock을 걸고 선착순 이벤트 참여자 수가 지정된 수보다 적다면, 선착순 당첨 정보를 DB에 저장하고
@@ -48,34 +48,34 @@ public class FcfsService {
      * 만약, 참여자 수가 총 당첨자 수와 같아졌으면, fcfsSettingManager의 setFcfsClosed를 true로 변환한다.
      * 2. setFcfsClosed가 true로 바뀌게 전에 요청이 들어왔다면, 선착순 실패 응답을 생성하여 반환한다.
      */
-    @EventLock(key = "FCFS_WINNER_#{#round}")
-    private FcfsResponseDto saveFcfsWinners(int userId, int round) {
-        Set<Integer> participantIds = eventLockRedisUtil.getAllDataAsSet(RedisLockPrefix.FCFS_LOCK_PREFIX.getPrefix() + round);
-
-        if (participantIds.size() < fcfsSettingManager.getWinnerNum() &&
-                !eventLockRedisUtil.isParticipantExists(RedisLockPrefix.FCFS_LOCK_PREFIX.getPrefix() + round, userId)) {
-            User user = userRepository.findById(userId)
-                    .orElseThrow(() -> {
-                        log.error("user not found in saveFcfsWinners method.");
-                        return new UserException(ErrorStatus._NOT_FOUND);
-                    });
-
-            Fcfs fcfs = Fcfs.builder()
-                    .user(user)
-                    .round(round)
-                    .build();
-            fcfsRepository.save(fcfs);
-
-            eventLockRedisUtil.incrementParticipantCount(RedisLockPrefix.FCFS_PARTICIPANT_COUNT_PREFIX.getPrefix() + round);
-            if (participantIds.size() + 1 == fcfsSettingManager.getWinnerNum()) {
-                fcfsSettingManager.setFcfsClosed(true);
-            }
-
-            return new FcfsSuccessResponseDto(1);
-        }
-
-        return new FcfsFailResponseDtoDto(1);
-    }
+//    @EventLock(key = "FCFS_WINNER_#{#round}")
+//    private FcfsResponseDto saveFcfsWinners(int userId, int round) {
+//        Set<Integer> participantIds = eventLockRedisUtil.getAllDataAsSet(RedisLockPrefix.FCFS_LOCK_PREFIX.getPrefix() + round);
+//
+//        if (participantIds.size() < fcfsSettingManager.getWinnerNum() &&
+//                !eventLockRedisUtil.isParticipantExists(RedisLockPrefix.FCFS_LOCK_PREFIX.getPrefix() + round, userId)) {
+//            User user = userRepository.findById(userId)
+//                    .orElseThrow(() -> {
+//                        log.error("user not found in saveFcfsWinners method.");
+//                        return new UserException(ErrorStatus._NOT_FOUND);
+//                    });
+//
+//            Fcfs fcfs = Fcfs.builder()
+//                    .user(user)
+//                    .round(round)
+//                    .build();
+//            fcfsRepository.save(fcfs);
+//
+//            eventLockRedisUtil.incrementParticipantCount(RedisLockPrefix.FCFS_PARTICIPANT_COUNT_PREFIX.getPrefix() + round);
+//            if (participantIds.size() + 1 == fcfsSettingManager.getWinnerNum()) {
+//                fcfsSettingManager.setFcfsClosed(true);
+//            }
+//
+//            return new FcfsSuccessResponseDto(1);
+//        }
+//
+//        return new FcfsFailResponseDtoDto(1);
+//    }
 
     private FcfsFailResponseDtoDto countFcfsParticipant(int round) {
         eventLockRedisUtil.incrementParticipantCount(RedisLockPrefix.FCFS_PARTICIPANT_COUNT_PREFIX.getPrefix() + round);

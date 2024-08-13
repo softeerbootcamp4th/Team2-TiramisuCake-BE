0a548ec3ab10cf455f1c3a4bb7f8b9a7c00f3d73
DrRivaski <leesm990708@gmail.com>
2024-08-13 11:16:39 +0900

    feat: 당첨 모달 dto 클래스 생성

---
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/DrawWinModalResponseDto.java
@@ -0,0 +1,13 @@
+package com.softeer.backend.fo_domain.draw.dto;
+
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+public class DrawWinModalResponseDto extends DrawModalResponseDto {
+    private String title; // 제목
+    private String subtitle; // 부제목
+    private String img; // 이미지 URL (S3 URL)
+    private String description; // 설명
+}

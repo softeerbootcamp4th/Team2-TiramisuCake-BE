a7acdaaa99327052556672bb0128c637aa9b12b4
hyeokson <chanhuk1765@gmail.com>
2024-08-13 21:39:03 +0900

    refactor: 클래스명 및 ThreadName prefix 수정

--- src/main/java/com/softeer/backend/global/config/schedular/SchedulerConfig.java
+++ src/main/java/com/softeer/backend/global/config/schedular/SchedulerConfig.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
-public class SchedularConfig {
+public class SchedulerConfig {
     @Bean
     public ThreadPoolTaskScheduler taskScheduler() {
         ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
-        taskScheduler.setPoolSize(2);
-        taskScheduler.setThreadNamePrefix("EventScheduler-");
+        taskScheduler.setPoolSize(1);
+        taskScheduler.setThreadNamePrefix("DbInsertScheduler-");
         return taskScheduler;
     }
 }

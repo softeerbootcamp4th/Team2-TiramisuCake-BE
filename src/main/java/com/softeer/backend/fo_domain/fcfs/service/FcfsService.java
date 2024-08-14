3754c3348ef2ee5a25822964bcf31cf8b90a8888
hyeokson <chanhuk1765@gmail.com>
2024-08-14 17:18:30 +0900

    feat: 선착순 퀴즈 페이지 정보를 제공하는 메서드 구현

--- src/main/java/com/softeer/backend/fo_domain/fcfs/service/FcfsService.java
+++ src/main/java/com/softeer/backend/fo_domain/fcfs/service/FcfsService.java
@@ -1,9 +1,11 @@
 package com.softeer.backend.fo_domain.fcfs.service;
 
-import com.softeer.backend.fo_domain.fcfs.dto.FcfsFailResponseDtoDto;
+import com.softeer.backend.fo_domain.fcfs.dto.*;
+import com.softeer.backend.fo_domain.fcfs.dto.result.FcfsFailResponseDto;
+import com.softeer.backend.fo_domain.fcfs.dto.result.FcfsResponseDto;
 import com.softeer.backend.fo_domain.fcfs.repository.FcfsRepository;
 import com.softeer.backend.fo_domain.user.repository.UserRepository;
-import com.softeer.backend.global.common.constant.RedisKeyPrefix;
+import com.softeer.backend.global.staticresources.util.StaticResourcesUtil;
 import com.softeer.backend.global.util.EventLockRedisUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,12 +23,39 @@ public class FcfsService {
     private final FcfsRepository fcfsRepository;
     private final EventLockRedisUtil eventLockRedisUtil;
     private final UserRepository userRepository;
+    private final StaticResourcesUtil staticResourcesUtil;
+
+
+    public FcfsPageResponseDto getFcfsPage(int round) {
+
+        QuizDto quiz = fcfsSettingManager.getQuiz(round);
+
+        return FcfsPageResponseDto.builder()
+                .answerWord(quiz.getAnswerWord())
+                .answerSentence(quiz.getAnswerSentence())
+                .startIndex(quiz.getStartIndex())
+                .endIndex(quiz.getEndIndex())
+                .quizDescription(staticResourcesUtil.getData("FCFS_QUIZ_DESCRIPTION"))
+                .build();
+    }
+
+    public FcfsPageResponseDto getFcfsTutorialPage() {
+        QuizDto tutorialQuiz = fcfsSettingManager.getTutorialQuiz();
+
+        return FcfsPageResponseDto.builder()
+                .answerWord(tutorialQuiz.getAnswerWord())
+                .answerSentence(tutorialQuiz.getAnswerSentence())
+                .startIndex(tutorialQuiz.getStartIndex())
+                .endIndex(tutorialQuiz.getEndIndex())
+                .quizDescription(staticResourcesUtil.getData("FCFS_QUIZ_DESCRIPTION"))
+                .build();
+    }
 
     /**
      * 1. 선착순 당첨자가 아직 다 결정되지 않았으면, 선착순 당첨 응답 생성 및 반환
      * 2. 선착순 당첨자가 다 결정됐다면, Redisson lock을 사용하지 않고 Redis에 저장된 선착순 이벤트 참여자 수를 1명씩 더한다.
      */
-//    public FcfsResponseDto handleFcfsEvent(int userId) {
+//    public FcfsResponseDto handleFcfsEvent(int userId, int round, String answer) {
 //        if (fcfsSettingManager.isFcfsClosed())
 //            return countFcfsParticipant(fcfsSettingManager.getRound());
 //
@@ -68,10 +97,24 @@ public class FcfsService {
 //        return new FcfsFailResponseDtoDto(1);
 //    }
 
-    private FcfsFailResponseDtoDto countFcfsParticipant(int round) {
-        eventLockRedisUtil.incrementData(RedisKeyPrefix.FCFS_PARTICIPANT_COUNT_PREFIX.getPrefix() + round);
+    public FcfsResponseDto getFcfsResult(boolean fcfsWin){
+//        if(fcfsWin){
+//            return FcfsSuccessResponseDto.builder()
+//                    .title(staticResourcesUtil.getData("FCFS_WINNER_TITLE"))
+//                    .subTitle(staticResourcesUtil.getData("FCFS_WINNER_SUBTITLE"))
+//                    .qrCode(staticResourcesUtil.getData("barcode_image"))
+//                    .codeWord(staticResourcesUtil.getData("FCFS_WINNER_CODE_WORD"))
+//                    .fcfsCode()
+//                    .expirationDate(staticResourcesUtil.getData("FCFS_WINNER_EXPIRY_DATE"))
+//                    .caution(staticResourcesUtil.getData("FCFS_WINNER_CAUTION"))
+//                    .build();
+//        }
 
-        return new FcfsFailResponseDtoDto(1);
+        return FcfsFailResponseDto.builder()
+                .title(staticResourcesUtil.getData("FCFS_LOSER_TITLE"))
+                .subTitle(staticResourcesUtil.getData("FCFS_LOSER_SUBTITLE"))
+                .caution(staticResourcesUtil.getData("FCFS_LOSER_CAUTION"))
+                .build();
     }
 
 }

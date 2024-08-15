c25ce27255a20d929c4dfb63f2f3c23ae8e8d207
DrRivaski <leesm990708@gmail.com>
2024-08-15 23:46:31 +0900

    feat: @Modifying 어노테이션 추가

--- src/main/java/com/softeer/backend/fo_domain/share/repository/ShareInfoRepository.java
+++ src/main/java/com/softeer/backend/fo_domain/share/repository/ShareInfoRepository.java
@@ -2,6 +2,7 @@ package com.softeer.backend.fo_domain.share.repository;
 
 import com.softeer.backend.fo_domain.share.domain.ShareInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,7 @@ import java.util.Optional;
 public interface ShareInfoRepository extends JpaRepository<ShareInfo, Integer> {
     Optional<ShareInfo> findShareInfoByUserId(Integer userId);
 
+    @Modifying
     @Query("UPDATE ShareInfo s SET s.remainDrawCount = s.remainDrawCount + 1 WHERE s.userId = :userId")
     void increaseRemainDrawCount(Integer userId);
 }

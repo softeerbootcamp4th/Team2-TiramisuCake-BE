cf53fbc7af024e5f0a0e9e18c1bc5cd564a47e29
DrRivaski <leesm990708@gmail.com>
2024-08-15 22:05:55 +0900

    feat: 세션에 공유 url을 설정하고, 헤더를 이용해 redirect하는 컨트롤러 메서드 구현

--- src/main/java/com/softeer/backend/fo_domain/share/controller/ShareController.java
+++ src/main/java/com/softeer/backend/fo_domain/share/controller/ShareController.java
@@ -5,8 +5,14 @@ import com.softeer.backend.fo_domain.share.service.ShareUrlInfoService;
 import com.softeer.backend.global.annotation.AuthInfo;
 import com.softeer.backend.global.common.response.ResponseDto;
 import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +23,17 @@ public class ShareController {
     @GetMapping("/share-shorten-url")
     public ResponseDto<ShareUrlInfoResponseDto> getShortenShareUrl(@Parameter(hidden = true) @AuthInfo Integer userId) {
         return shareUrlInfoService.getShortenShareUrl(userId);
+    }
+
+    @GetMapping("/share/{shareUrl}")
+    public ResponseEntity<Void> redirectWithShareUrl(@PathVariable String shareUrl, HttpServletRequest request) {
+        // session을 이용해 공유 url 저장
+        HttpSession session = request.getSession();
+        session.setAttribute("shareUrl", shareUrl);
+
+        // 헤더를 이용해 redirect
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/");
+        return new ResponseEntity<>(headers, HttpStatus.FOUND); // HTTP 302 Found 응답
     }
 }

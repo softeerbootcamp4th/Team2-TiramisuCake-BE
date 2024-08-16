1759619f6be78559edfc670286a8bdf43a8cd478
DrRivaski <leesm990708@gmail.com>
2024-08-16 13:47:02 +0900

    refactor: service에서 Dto만 반환하도록 수정

--- src/main/java/com/softeer/backend/fo_domain/share/service/ShareUrlInfoService.java
+++ src/main/java/com/softeer/backend/fo_domain/share/service/ShareUrlInfoService.java
@@ -4,7 +4,6 @@ import com.softeer.backend.fo_domain.share.dto.ShareUrlInfoResponseDto;
 import com.softeer.backend.fo_domain.share.exception.ShareInfoException;
 import com.softeer.backend.fo_domain.share.repository.ShareUrlInfoRepository;
 import com.softeer.backend.global.common.code.status.ErrorStatus;
-import com.softeer.backend.global.common.response.ResponseDto;
 import com.softeer.backend.global.staticresources.util.StaticResourcesUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,12 +14,12 @@ public class ShareUrlInfoService {
     private final ShareUrlInfoRepository shareUrlInfoRepository;
     private final StaticResourcesUtil staticResourcesUtil;
 
-    public ResponseDto<ShareUrlInfoResponseDto> getShortenShareUrl(Integer userId) {
+    public ShareUrlInfoResponseDto getShortenShareUrl(Integer userId) {
         if (userId == null) {
             // 로그인하지 않은 사용자
-            return ResponseDto.onSuccess(ShareUrlInfoResponseDto.builder()
+            return ShareUrlInfoResponseDto.builder()
                     .shareUrl(staticResourcesUtil.getData("NON_USER_SHARE_URL"))
-                    .build());
+                    .build();
         } else {
             // 로그인한 사용자
             String shareUrl = shareUrlInfoRepository.findShareUrlByUserId(userId).orElseThrow(
@@ -28,9 +27,9 @@ public class ShareUrlInfoService {
             );
 
             // DB에 이미 생성된 단축 url 반환
-            return ResponseDto.onSuccess(ShareUrlInfoResponseDto.builder()
+            return ShareUrlInfoResponseDto.builder()
                     .shareUrl(staticResourcesUtil.getData("BASE_URL") + shareUrl)
-                    .build());
+                    .build();
         }
     }
 }

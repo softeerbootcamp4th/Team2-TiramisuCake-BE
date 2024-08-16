fb3125580abed335c4b3d362082dca1f13c7c220
hyeokson <chanhuk1765@gmail.com>
2024-08-16 18:50:27 +0900

    feat: json property 설정

--- src/main/java/com/softeer/backend/fo_domain/fcfs/dto/result/FcfsResultResponseDto.java
+++ src/main/java/com/softeer/backend/fo_domain/fcfs/dto/result/FcfsResultResponseDto.java
@@ -1,5 +1,6 @@
 package com.softeer.backend.fo_domain.fcfs.dto.result;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -8,6 +9,7 @@ import lombok.*;
 @Getter
 public class FcfsResultResponseDto {
 
+    @JsonProperty("isFcfsWinner")
     private boolean isFcfsWinner;
 
     private FcfsResult fcfsResult;

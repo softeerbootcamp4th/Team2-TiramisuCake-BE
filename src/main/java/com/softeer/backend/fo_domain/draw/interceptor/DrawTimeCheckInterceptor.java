849eee822ca09c9522bff3ebea3bf20b822f25f4
DrRivaski <leesm990708@gmail.com>
2024-08-18 01:06:47 +0900

    feat: 참여 시각 체크하는 로직 추가

--- src/main/java/com/softeer/backend/fo_domain/draw/interceptor/DrawTimeCheckInterceptor.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/interceptor/DrawTimeCheckInterceptor.java
@@ -1,9 +1,47 @@
 package com.softeer.backend.fo_domain.draw.interceptor;
 
+import com.softeer.backend.fo_domain.draw.service.DrawSettingManager;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
-public class DrawTimeCheckInterceptor {
+public class DrawTimeCheckInterceptor implements HandlerInterceptor {
+    private final DrawSettingManager drawSettingManager;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (CorsUtils.isPreFlightRequest(request))
+            return true;
+
+        return isAvailableTime();
+    }
+
+    private boolean isAvailableTime() {
+        LocalDateTime now = LocalDateTime.now();
+
+        return compareDate(now) && compareTime(now);
+    }
+
+    private boolean compareDate(LocalDateTime now) {
+        LocalDateTime startDateTime = drawSettingManager.getStartDate().atStartOfDay();
+        LocalDateTime endDateTime = drawSettingManager.getEndDate().atStartOfDay();
+
+        return now.isAfter(startDateTime) && now.isBefore(endDateTime);
+    }
+
+    private boolean compareTime(LocalDateTime now) {
+        LocalDate nowDate = now.toLocalDate();
+        LocalDateTime startTimeAsDateTime = LocalDateTime.of(nowDate, drawSettingManager.getStartTime());
+        LocalDateTime endTimeAsDateTime = LocalDateTime.of(nowDate, drawSettingManager.getEndTime());
+
+        return (now.isAfter(startTimeAsDateTime) && now.isBefore(endTimeAsDateTime));
+    }
 }

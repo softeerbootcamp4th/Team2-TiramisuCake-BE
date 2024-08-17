aa3f1097fcd97f1ea90f5604aa866d8e9485ce75
DrRivaski <leesm990708@gmail.com>
2024-08-18 00:59:32 +0900

    feat: 참여 시각 체크하는 인터셉터 추가

---
+++ src/main/java/com/softeer/backend/fo_domain/draw/interceptor/DrawTimeCheckInterceptor.java
@@ -0,0 +1,9 @@
+package com.softeer.backend.fo_domain.draw.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DrawTimeCheckInterceptor {
+}

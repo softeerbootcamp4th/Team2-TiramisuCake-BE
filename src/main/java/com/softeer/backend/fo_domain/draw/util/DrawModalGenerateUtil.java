baf1e8a39c2752cf466eb61e665202b146145941
DrRivaski <leesm990708@gmail.com>
2024-08-17 22:23:47 +0900

    refactor: 7일 연속 참여 모달 제거, WinModal과 통합해서 generate하도록 수정

--- src/main/java/com/softeer/backend/fo_domain/draw/util/DrawModalGenerateUtil.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/util/DrawModalGenerateUtil.java
@@ -1,6 +1,7 @@
 package com.softeer.backend.fo_domain.draw.util;
 
 import com.softeer.backend.fo_domain.draw.dto.main.DrawMainFullAttendResponseDto;
+import com.softeer.backend.fo_domain.draw.dto.modal.WinModal;
 import com.softeer.backend.fo_domain.draw.dto.participate.DrawWinModalResponseDto;
 import com.softeer.backend.fo_domain.draw.dto.result.DrawHistoryWinnerResponseDto;
 import com.softeer.backend.global.staticresources.util.StaticResourcesUtil;
@@ -17,11 +18,11 @@ public class DrawModalGenerateUtil {
      *
      * @return FullAttendModal 반환
      */
-    public DrawMainFullAttendResponseDto.FullAttendModal generateFullAttendModal() {
-        return DrawMainFullAttendResponseDto.FullAttendModal.builder()
+    public WinModal generateFullAttendModal() {
+        return WinModal.builder()
                 .title(staticResourcesUtil.getData("FULL_ATTEND_MODAL_TITLE"))
                 .subtitle(staticResourcesUtil.getData("FULL_ATTEND_MODAL_SUBTITLE"))
-                .image(staticResourcesUtil.getData("attendance_reward_image"))
+                .img(staticResourcesUtil.getData("attendance_reward_image"))
                 .description(staticResourcesUtil.getData("FULL_ATTEND_MODAL_DESCRIPTION"))
                 .build();
     }
@@ -29,21 +30,23 @@ public class DrawModalGenerateUtil {
     /**
      * @return 등수에 따른 WinModal을 반환
      */
-    public DrawWinModalResponseDto.WinModal generateWinModal(int ranking) {
+    public WinModal generateWinModal(int ranking) {
         if (ranking == 1) {
             return generateFirstWinModal();
         } else if (ranking == 2) {
             return generateSecondWinModal();
-        } else {
+        } else if (ranking == 3) {
             return generateThirdWinModal();
+        } else {
+            return generateFullAttendModal();
         }
     }
 
     /**
      * @return 1등 WinModal 반환
      */
-    private DrawWinModalResponseDto.WinModal generateFirstWinModal() {
-        return DrawWinModalResponseDto.WinModal.builder()
+    private WinModal generateFirstWinModal() {
+        return WinModal.builder()
                 .title(staticResourcesUtil.getData("DRAW_WINNER_MODAL_TITLE"))
                 .subtitle(staticResourcesUtil.getData("DRAW_FIRST_WINNER_MODAL_SUBTITLE"))
                 .img(staticResourcesUtil.getData("draw_reward_image_1"))
@@ -54,8 +57,8 @@ public class DrawModalGenerateUtil {
     /**
      * @return 2등 WinModal 반환
      */
-    private DrawWinModalResponseDto.WinModal generateSecondWinModal() {
-        return DrawWinModalResponseDto.WinModal.builder()
+    private WinModal generateSecondWinModal() {
+        return WinModal.builder()
                 .title(staticResourcesUtil.getData("DRAW_WINNER_MODAL_TITLE"))
                 .subtitle(staticResourcesUtil.getData("DRAW_SECOND_WINNER_MODAL_SUBTITLE"))
                 .img(staticResourcesUtil.getData("draw_reward_image_2"))
@@ -66,49 +69,49 @@ public class DrawModalGenerateUtil {
     /**
      * @return 3등 WinModal 반환
      */
-    private DrawWinModalResponseDto.WinModal generateThirdWinModal() {
-        return DrawWinModalResponseDto.WinModal.builder()
+    private WinModal generateThirdWinModal() {
+        return WinModal.builder()
                 .title(staticResourcesUtil.getData("DRAW_WINNER_MODAL_TITLE"))
                 .subtitle(staticResourcesUtil.getData("DRAW_THIRD_WINNER_MODAL_SUBTITLE"))
                 .img(staticResourcesUtil.getData("draw_reward_image_3"))
                 .description(staticResourcesUtil.getData("DRAW_WINNER_MODAL_DESCRIPTION"))
                 .build();
     }
-
-    public DrawHistoryWinnerResponseDto.WinModal generateWinModalForHistory(int ranking) {
-        if (ranking == 1) {
-            return generateFirstWinModalForHistory();
-        } else if (ranking == 2) {
-            return generateSecondWinModalForHistory();
-        } else {
-            return generateThirdWinModalForHistory();
-        }
-    }
-
-    private DrawHistoryWinnerResponseDto.WinModal generateFirstWinModalForHistory() {
-        return DrawHistoryWinnerResponseDto.WinModal.builder()
-                .title(staticResourcesUtil.getData("DRAW_WINNER_MODAL_TITLE"))
-                .subtitle(staticResourcesUtil.getData("DRAW_FIRST_WINNER_MODAL_SUBTITLE"))
-                .img(staticResourcesUtil.getData("draw_reward_image_1"))
-                .description(staticResourcesUtil.getData("DRAW_WINNER_MODAL_DESCRIPTION"))
-                .build();
-    }
-
-    private DrawHistoryWinnerResponseDto.WinModal generateSecondWinModalForHistory() {
-        return DrawHistoryWinnerResponseDto.WinModal.builder()
-                .title(staticResourcesUtil.getData("DRAW_WINNER_MODAL_TITLE"))
-                .subtitle(staticResourcesUtil.getData("DRAW_SECOND_WINNER_MODAL_SUBTITLE"))
-                .img(staticResourcesUtil.getData("draw_reward_image_2"))
-                .description(staticResourcesUtil.getData("DRAW_WINNER_MODAL_DESCRIPTION"))
-                .build();
-    }
-
-    private DrawHistoryWinnerResponseDto.WinModal generateThirdWinModalForHistory() {
-        return DrawHistoryWinnerResponseDto.WinModal.builder()
-                .title(staticResourcesUtil.getData("DRAW_WINNER_MODAL_TITLE"))
-                .subtitle(staticResourcesUtil.getData("DRAW_THIRD_WINNER_MODAL_SUBTITLE"))
-                .img(staticResourcesUtil.getData("draw_reward_image_3"))
-                .description(staticResourcesUtil.getData("DRAW_WINNER_MODAL_DESCRIPTION"))
-                .build();
-    }
+//
+//    public DrawHistoryWinnerResponseDto.WinModal generateWinModalForHistory(int ranking) {
+//        if (ranking == 1) {
+//            return generateFirstWinModalForHistory();
+//        } else if (ranking == 2) {
+//            return generateSecondWinModalForHistory();
+//        } else {
+//            return generateThirdWinModalForHistory();
+//        }
+//    }
+//
+//    private DrawHistoryWinnerResponseDto.WinModal generateFirstWinModalForHistory() {
+//        return DrawHistoryWinnerResponseDto.WinModal.builder()
+//                .title(staticResourcesUtil.getData("DRAW_WINNER_MODAL_TITLE"))
+//                .subtitle(staticResourcesUtil.getData("DRAW_FIRST_WINNER_MODAL_SUBTITLE"))
+//                .img(staticResourcesUtil.getData("draw_reward_image_1"))
+//                .description(staticResourcesUtil.getData("DRAW_WINNER_MODAL_DESCRIPTION"))
+//                .build();
+//    }
+//
+//    private DrawHistoryWinnerResponseDto.WinModal generateSecondWinModalForHistory() {
+//        return DrawHistoryWinnerResponseDto.WinModal.builder()
+//                .title(staticResourcesUtil.getData("DRAW_WINNER_MODAL_TITLE"))
+//                .subtitle(staticResourcesUtil.getData("DRAW_SECOND_WINNER_MODAL_SUBTITLE"))
+//                .img(staticResourcesUtil.getData("draw_reward_image_2"))
+//                .description(staticResourcesUtil.getData("DRAW_WINNER_MODAL_DESCRIPTION"))
+//                .build();
+//    }
+//
+//    private DrawHistoryWinnerResponseDto.WinModal generateThirdWinModalForHistory() {
+//        return DrawHistoryWinnerResponseDto.WinModal.builder()
+//                .title(staticResourcesUtil.getData("DRAW_WINNER_MODAL_TITLE"))
+//                .subtitle(staticResourcesUtil.getData("DRAW_THIRD_WINNER_MODAL_SUBTITLE"))
+//                .img(staticResourcesUtil.getData("draw_reward_image_3"))
+//                .description(staticResourcesUtil.getData("DRAW_WINNER_MODAL_DESCRIPTION"))
+//                .build();
+//    }
 }

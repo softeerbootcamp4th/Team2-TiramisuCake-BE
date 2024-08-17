eea508854d42eaa84db94bb11e6ac3bf6e1b1767
DrRivaski <leesm990708@gmail.com>
2024-08-17 22:26:15 +0900

    chore: 사용하지 않는 주석 삭제

--- src/main/java/com/softeer/backend/fo_domain/draw/util/DrawModalGenerateUtil.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/util/DrawModalGenerateUtil.java
@@ -11,20 +11,6 @@ public class DrawModalGenerateUtil {
     private final StaticResourcesUtil staticResourcesUtil;
 
     /**
-     * 7일 연속 출석자 상품 정보 반환 메서드
-     *
-     * @return FullAttendModal 반환
-     */
-    public WinModal generateFullAttendModal() {
-        return WinModal.builder()
-                .title(staticResourcesUtil.getData("FULL_ATTEND_MODAL_TITLE"))
-                .subtitle(staticResourcesUtil.getData("FULL_ATTEND_MODAL_SUBTITLE"))
-                .img(staticResourcesUtil.getData("attendance_reward_image"))
-                .description(staticResourcesUtil.getData("FULL_ATTEND_MODAL_DESCRIPTION"))
-                .build();
-    }
-
-    /**
      * @return 등수에 따른 WinModal을 반환
      */
     public WinModal generateWinModal(int ranking) {
@@ -74,41 +60,18 @@ public class DrawModalGenerateUtil {
                 .description(staticResourcesUtil.getData("DRAW_WINNER_MODAL_DESCRIPTION"))
                 .build();
     }
-//
-//    public DrawHistoryWinnerResponseDto.WinModal generateWinModalForHistory(int ranking) {
-//        if (ranking == 1) {
-//            return generateFirstWinModalForHistory();
-//        } else if (ranking == 2) {
-//            return generateSecondWinModalForHistory();
-//        } else {
-//            return generateThirdWinModalForHistory();
-//        }
-//    }
-//
-//    private DrawHistoryWinnerResponseDto.WinModal generateFirstWinModalForHistory() {
-//        return DrawHistoryWinnerResponseDto.WinModal.builder()
-//                .title(staticResourcesUtil.getData("DRAW_WINNER_MODAL_TITLE"))
-//                .subtitle(staticResourcesUtil.getData("DRAW_FIRST_WINNER_MODAL_SUBTITLE"))
-//                .img(staticResourcesUtil.getData("draw_reward_image_1"))
-//                .description(staticResourcesUtil.getData("DRAW_WINNER_MODAL_DESCRIPTION"))
-//                .build();
-//    }
-//
-//    private DrawHistoryWinnerResponseDto.WinModal generateSecondWinModalForHistory() {
-//        return DrawHistoryWinnerResponseDto.WinModal.builder()
-//                .title(staticResourcesUtil.getData("DRAW_WINNER_MODAL_TITLE"))
-//                .subtitle(staticResourcesUtil.getData("DRAW_SECOND_WINNER_MODAL_SUBTITLE"))
-//                .img(staticResourcesUtil.getData("draw_reward_image_2"))
-//                .description(staticResourcesUtil.getData("DRAW_WINNER_MODAL_DESCRIPTION"))
-//                .build();
-//    }
-//
-//    private DrawHistoryWinnerResponseDto.WinModal generateThirdWinModalForHistory() {
-//        return DrawHistoryWinnerResponseDto.WinModal.builder()
-//                .title(staticResourcesUtil.getData("DRAW_WINNER_MODAL_TITLE"))
-//                .subtitle(staticResourcesUtil.getData("DRAW_THIRD_WINNER_MODAL_SUBTITLE"))
-//                .img(staticResourcesUtil.getData("draw_reward_image_3"))
-//                .description(staticResourcesUtil.getData("DRAW_WINNER_MODAL_DESCRIPTION"))
-//                .build();
-//    }
+
+    /**
+     * 7일 연속 출석자 상품 정보 반환 메서드
+     *
+     * @return FullAttendModal 반환
+     */
+    public WinModal generateFullAttendModal() {
+        return WinModal.builder()
+                .title(staticResourcesUtil.getData("FULL_ATTEND_MODAL_TITLE"))
+                .subtitle(staticResourcesUtil.getData("FULL_ATTEND_MODAL_SUBTITLE"))
+                .img(staticResourcesUtil.getData("attendance_reward_image"))
+                .description(staticResourcesUtil.getData("FULL_ATTEND_MODAL_DESCRIPTION"))
+                .build();
+    }
 }

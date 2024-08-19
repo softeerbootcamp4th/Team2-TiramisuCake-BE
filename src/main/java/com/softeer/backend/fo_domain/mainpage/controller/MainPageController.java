31d30b2e3b5c26a07bf9efc9f6fb21f895be1d05
hyeokson <chanhuk1765@gmail.com>
2024-08-19 16:54:11 +0900

    feat: 이벤트 페이지 정보를 응답하는 메서드 구현

--- src/main/java/com/softeer/backend/fo_domain/mainpage/controller/MainPageController.java
+++ src/main/java/com/softeer/backend/fo_domain/mainpage/controller/MainPageController.java
@@ -1,13 +1,19 @@
 package com.softeer.backend.fo_domain.mainpage.controller;
 
 import com.softeer.backend.fo_domain.mainpage.dto.MainPageCarResponseDto;
-import com.softeer.backend.fo_domain.mainpage.dto.MainPageEventResponseDto;
+import com.softeer.backend.fo_domain.mainpage.dto.MainPageEventInfoResponseDto;
+import com.softeer.backend.fo_domain.mainpage.dto.MainPageEventStaticResponseDto;
 import com.softeer.backend.fo_domain.mainpage.service.MainPageService;
 import com.softeer.backend.global.common.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,19 +22,31 @@ public class MainPageController {
 
     private final MainPageService mainPageService;
 
-    @GetMapping("/event")
-    public ResponseDto<MainPageEventResponseDto> getEventPage(){
-        MainPageEventResponseDto mainPageEventResponseDto = mainPageService.getEventPage();
+    @GetMapping("/event/static")
+    public ResponseEntity<ResponseDto<MainPageEventStaticResponseDto>> getEventPageStatic(){
+        MainPageEventStaticResponseDto mainPageEventStaticResponseDto= mainPageService.getEventPageStatic();
 
-        return ResponseDto.onSuccess(mainPageEventResponseDto);
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(1, TimeUnit.DAYS).cachePublic()) // 1일 동안 public 캐싱
+                .body(ResponseDto.onSuccess(mainPageEventStaticResponseDto));
+    }
+
+    @GetMapping("/event/info")
+    public ResponseDto<MainPageEventInfoResponseDto> getEventPageInfo(){
+
+        MainPageEventInfoResponseDto mainPageEventInfoResponseDto = mainPageService.getEventPageInfo();
+
+        return ResponseDto.onSuccess(mainPageEventInfoResponseDto);
     }
 
     @GetMapping("/car")
-    public ResponseDto<MainPageCarResponseDto> getCarPage(){
+    public ResponseEntity<ResponseDto<MainPageCarResponseDto>> getCarPage(){
 
         MainPageCarResponseDto mainPageCarResponseDto = mainPageService.getCarPage();
 
-        return ResponseDto.onSuccess(mainPageCarResponseDto);
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(1, TimeUnit.DAYS).cachePublic()) // 1일 동안 public 캐싱
+                .body(ResponseDto.onSuccess(mainPageCarResponseDto));
     }
 
 }

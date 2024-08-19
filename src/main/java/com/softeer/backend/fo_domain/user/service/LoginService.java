f0e8d4b62cb7f80e332ee432273bf5d734137c19
DrRivaski <leesm990708@gmail.com>
2024-08-19 14:07:21 +0900

    feat: 사용하지 않는 import문 삭제

--- src/main/java/com/softeer/backend/fo_domain/user/service/LoginService.java
+++ src/main/java/com/softeer/backend/fo_domain/user/service/LoginService.java
@@ -23,15 +23,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class LoginService {
-
     private final UserRepository userRepository;
     private final ShareInfoRepository shareInfoRepository;
     private final ShareUrlInfoRepository shareUrlInfoRepository;

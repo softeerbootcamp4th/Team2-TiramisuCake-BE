74b810d0d53a53d7bb06809a85a06db4cfdb94d6
hyeokson <chanhuk1765@gmail.com>
2024-08-19 16:51:52 +0900

    refactor: 문자열을 상수로 관리

--- src/main/java/com/softeer/backend/fo_domain/share/service/ShareUrlInfoService.java
+++ src/main/java/com/softeer/backend/fo_domain/share/service/ShareUrlInfoService.java
@@ -4,21 +4,22 @@ import com.softeer.backend.fo_domain.share.dto.ShareUrlInfoResponseDto;
 import com.softeer.backend.fo_domain.share.exception.ShareInfoException;
 import com.softeer.backend.fo_domain.share.repository.ShareUrlInfoRepository;
 import com.softeer.backend.global.common.code.status.ErrorStatus;
-import com.softeer.backend.global.staticresources.util.StaticResourcesUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ShareUrlInfoService {
+    public static final String NON_USER_SHARE_URL = "https://softeer.site";
+    public static final String BASE_URL = "https://softeer.site/share/";
+
     private final ShareUrlInfoRepository shareUrlInfoRepository;
-    private final StaticResourcesUtil staticResourcesUtil;
 
     public ShareUrlInfoResponseDto getShortenShareUrl(Integer userId) {
         if (userId == null) {
             // 로그인하지 않은 사용자
             return ShareUrlInfoResponseDto.builder()
-                    .shareUrl(staticResourcesUtil.getData("NON_USER_SHARE_URL"))
+                    .shareUrl(NON_USER_SHARE_URL)
                     .build();
         } else {
             // 로그인한 사용자
@@ -28,7 +29,7 @@ public class ShareUrlInfoService {
 
             // DB에 이미 생성된 단축 url 반환
             return ShareUrlInfoResponseDto.builder()
-                    .shareUrl(staticResourcesUtil.getData("BASE_URL") + shareUrl)
+                    .shareUrl(BASE_URL + shareUrl)
                     .build();
         }
     }

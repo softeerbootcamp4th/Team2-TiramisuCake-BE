9f9eba313aecfcf3fba1af702068127e88a91838
hyeokson <chanhuk1765@gmail.com>
2024-08-21 21:04:36 +0900

    chore: 패키지 변경

--- src/test/java/com/softeer/backend/bo_domain/admin/service/IndicatorPageServiceTest.java
+++ src/test/java/com/softeer/backend/bo_domain/admin/service/IndicatorPageServiceTest.java
@@ -1,4 +1,4 @@
-package com.softeer.backend.fo_domain.admin.service;
+package com.softeer.backend.bo_domain.admin.service;
 
 import com.softeer.backend.bo_domain.admin.dto.indicator.EventIndicatorResponseDto;
 import com.softeer.backend.bo_domain.admin.service.IndicatorPageService;

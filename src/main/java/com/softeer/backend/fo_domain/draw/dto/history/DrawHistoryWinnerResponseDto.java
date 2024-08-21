7e70ed66da82884ca1d6e6ffe61f38d7bca4e047
DrRivaski <leesm990708@gmail.com>
2024-08-21 16:34:59 +0900

    chore: 사용하지 않는 import문 삭제

--- src/main/java/com/softeer/backend/fo_domain/draw/dto/history/DrawHistoryWinnerResponseDto.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/dto/history/DrawHistoryWinnerResponseDto.java
@@ -1,6 +1,5 @@
 package com.softeer.backend.fo_domain.draw.dto.history;
 
-import com.softeer.backend.fo_domain.draw.dto.modal.WinModal;
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
 

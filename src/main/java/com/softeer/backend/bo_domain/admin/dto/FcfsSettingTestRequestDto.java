0374301b63bd17234d3b2a05aa02cd23e34f143f
hyeokson <chanhuk1765@gmail.com>
2024-08-22 17:07:50 +0900

    rebase: 원본 repo develop 브랜치와 rebase

--- src/main/java/com/softeer/backend/bo_domain/admin/dto/FcfsSettingTestRequestDto.java
+++ src/main/java/com/softeer/backend/bo_domain/admin/dto/FcfsSettingTestRequestDto.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public class FcfsSettingTestRequestDto {
 
     private int round;
-  
+
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime startTime;
 

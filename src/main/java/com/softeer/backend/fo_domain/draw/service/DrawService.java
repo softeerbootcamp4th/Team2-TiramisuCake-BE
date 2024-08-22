92ac86dd9869775ca56918d56d4bc903a4f7dd2e
DrRivaski <leesm990708@gmail.com>
2024-08-22 10:12:14 +0900

    test: import문 추가

--- src/main/java/com/softeer/backend/fo_domain/draw/service/DrawService.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/service/DrawService.java
@@ -9,6 +9,8 @@ import com.softeer.backend.fo_domain.draw.dto.history.DrawHistoryResponseDto;
 import com.softeer.backend.fo_domain.draw.exception.DrawException;
 import com.softeer.backend.fo_domain.draw.repository.DrawParticipationInfoRepository;
 import com.softeer.backend.fo_domain.draw.repository.DrawRepository;
+import com.softeer.backend.fo_domain.draw.test.DrawDatabaseTest;
+import com.softeer.backend.fo_domain.draw.test.DrawDatabaseUtil;
 import com.softeer.backend.fo_domain.draw.util.DrawAttendanceCountUtil;
 import com.softeer.backend.fo_domain.draw.util.DrawResponseGenerateUtil;
 import com.softeer.backend.fo_domain.draw.util.DrawUtil;
@@ -39,6 +41,7 @@ public class DrawService {
     private final DrawSettingManager drawSettingManager;
     private final DrawDatabaseUtil drawDatabaseUtil;
     private final DrawDatabaseTest drawDatabaseTest;
+    private final DrawRepository drawRepository;
 
     /**
      * 1. 연속 참여일수 조회

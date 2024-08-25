11bf84b9722bd982d821fb7e05b94c7aae833c4f
DrRivaski <leesm990708@gmail.com>
2024-08-26 03:28:07 +0900

    fix: 당첨 로직 수정
    - 당첨자 수 누적해서 계산하도록 수정

--- src/main/java/com/softeer/backend/fo_domain/draw/controller/DrawController.java
+++ src/main/java/com/softeer/backend/fo_domain/draw/controller/DrawController.java
@@ -7,9 +7,6 @@ import com.softeer.backend.fo_domain.draw.service.DrawService;
 import com.softeer.backend.global.annotation.AuthInfo;
 import com.softeer.backend.global.common.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,17 +31,7 @@ public class DrawController {
      * 추첨 이벤트 참여 메서드
      */
     @PostMapping("/event/draw")
-    public ResponseEntity<Void> participateDrawEvent() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Location", "/event/draw-result");
-        return new ResponseEntity<>(headers, HttpStatus.FOUND); // HTTP 302 Found 응답
-    }
-
-    /**
-     * 추첨 이벤트 결과 반환하는 메서드
-     */
-    @GetMapping("/event/draw-result")
-    public ResponseDto<DrawModalResponseDto> getDrawResult(@AuthInfo Integer userId) {
+    public ResponseDto<DrawModalResponseDto> participateDrawEvent(@AuthInfo Integer userId) {
         return ResponseDto.onSuccess(drawService.participateDrawEvent(userId));
     }
 

9bafec9e1c8e91a2104c3f732e833cfe4960b550
hyeokson <chanhuk1765@gmail.com>
2024-08-26 03:24:28 +0900

    refactor: 메서드명 변경

--- src/main/java/com/softeer/backend/fo_domain/comment/repository/CommentRepository.java
+++ src/main/java/com/softeer/backend/fo_domain/comment/repository/CommentRepository.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
-    Page<Comment> findAllByIdLessThanEqualOrderByIdDesc(Integer id, Pageable pageable);
+    Page<Comment> findAllByIdLessThanOrderByIdDesc(Integer id, Pageable pageable);
 
     Page<Comment> findAllByOrderByIdDesc(Pageable pageable);
 }
